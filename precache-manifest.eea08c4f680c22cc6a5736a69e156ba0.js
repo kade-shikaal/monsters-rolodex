428fe8a53915ab31fa94a656918e5daee96e410e
kade-shikaal <skadebis@gmail.com>
2019-08-12 11:19:17 +0200

    Updates

---
+++ precache-manifest.eea08c4f680c22cc6a5736a69e156ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d904921b7c8d49123999353840d08b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "afcaf0b5cc0abf7e79c7",
+    "url": "/monsters-rolodex/static/css/main.3c5a33cc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "afcaf0b5cc0abf7e79c7",
+    "url": "/monsters-rolodex/static/js/main.d1ace0cf.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
